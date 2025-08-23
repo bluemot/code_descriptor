76b567e467fe50bb50bc40052938ed8ed4fdea84
tomlin <tomlin@realtek.com>
2025-08-23 22:19:33 +0800

    add frontend files

---
+++ scripts/run_server.py
@@ -0,0 +1,23 @@
+import subprocess
+import time
+
+fail_count = 0
+while True:
+    error_occurred = False
+    try:
+        result = subprocess.run(["uvicorn", "backend.main:app"]);
+    except KeyboardInterrupt:
+        error_occurred = True
+    except Exception:
+        error_occurred = True
+    else:
+        if result.returncode == 0:
+            break
+        else:
+            error_occurred = True
+    if error_occurred:
+        fail_count += 1
+        if fail_count >= 10:
+            raise RuntimeError("Server crashed 10 times in a row, aborting.")
+        print("[runner] server crashed, restarting...")
+        time.sleep(1)
